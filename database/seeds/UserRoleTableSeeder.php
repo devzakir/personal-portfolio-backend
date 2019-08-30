418b4e6cb3386ce7589fe1084932e6d9d9cdfe36
Zakir Hossen <web.zakirbd@gmail.com>
2019-08-30 12:28:22 -0700

    laravel 5.8 project added, created user role model, seeder, migration

---
+++ database/seeds/UserRoleTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserRoleTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\UserRole::create([
+            'name' => 'Super Admin',
+            'role_id' => 1,
+        ]);
+
+        App\UserRole::create([
+            'name' => 'User',
+            'role_id' => 2,
+        ]);
+    }
+}
