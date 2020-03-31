4836990f964432ffb9c5738266377aef884a84d7
Zakir Hossen <web.zakirbd@gmail.com>
2020-03-31 08:52:29 +0600

    setting seeder udpate

--- database/seeds/SettingTableSeeder.php
+++ database/seeds/SettingTableSeeder.php
@@ -15,6 +15,15 @@ class SettingTableSeeder extends Seeder
         Setting::create([
             'name' => 'Example',
             'copyright' => 'Copyright &copy; Example 2020',
+            'phone' => '+880 1794 1724 79',
+            'email' => 'web.zakirbd@gmail.com',
+            'address' => 'Adabor, Dhaka-1207, Bangladesh',
+            'facebook' => 'https://facebook.com/devzakir',
+            'github' => 'https://github.com/devzakir',
+            'linkedin' => 'https://linkedin.com/in/devzakir',
+            'quora' => 'https://www.quora.com/profile/Zakir-Hossen-16',
+            'skype' => 'web.zakirbd',
+            'stackoverflow' => 'https://stackoverflow.com/users/9127475/devzakir',
         ]);
     }
 }
